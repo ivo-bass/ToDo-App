8a26e31d64eb8347d0e946e69863c0c171ea9755
ivo_bass <ivailo.ignatoff@gmail.com>
2021-06-03 21:56:20 +0300

    added foreign key owner to Todo

--- todo_app/todos/models/__init__.py
+++ todo_app/todos/models/__init__.py
@@ -1,1 +1,2 @@
 from .todo import Todo
+from .person import Person
--- todo_app/todos/models/todo.py
+++ todo_app/todos/models/todo.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+from .person import Person
+
 
 class Todo(models.Model):
     text = models.CharField(max_length=30)
     state = models.BooleanField(default=False)
-    person_responsible = models.CharField(
-        max_length=30,
+    owner = models.ForeignKey(
+        Person,
+        on_delete=models.CASCADE,
         null=True
     )
