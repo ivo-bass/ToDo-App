d318b5ce9f686d2c1f4dcef5a7e79da8d815bae1
ivo-bass <ivailo.ignatoff@gmail.com>
2021-06-03 21:39:38 +0300

    models to module not a file

--- todo_app/todos/models/todo.py
+++ todo_app/todos/models/todo.py
@@ -4,3 +4,7 @@ from django.db import models
 class Todo(models.Model):
     text = models.CharField(max_length=30)
     state = models.BooleanField(default=False)
+    person_responsible = models.CharField(
+        max_length=30,
+        null=True
+    )
