c3c216db209527f262e36d5fd2cc8cb98c2b5601
ivo-bass <ivailo.ignatoff@gmail.com>
2021-06-06 00:21:30 +0300

    added filter and sort to admin

--- todo_app/todos/admin.py
+++ todo_app/todos/admin.py
@@ -4,10 +4,9 @@ from .models import Todo, Priority, Category
 
 
 class TodoAdmin(admin.ModelAdmin):
-    # list_display = ['title', 'state', 'priority', 'category', 'due_date', ]
-    # list_filter = ['priority', 'state', 'category', ]
-    # sortable_by = ['title', 'state', 'priority', 'category', 'due_date', ]
-    pass
+    list_display = ['title', 'state', 'priority', 'category', 'due_date', ]
+    list_filter = ['priority', 'state', 'category', ]
+    sortable_by = ['title', 'state', 'priority', 'category', 'due_date', ]
 
 
 admin.site.register(Todo, TodoAdmin)
